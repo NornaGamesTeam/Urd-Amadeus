d57ca6985fd59e2cbedf7f5905e27998a9755df9
AntonioZu <antoniojesusnc@gmail.com>
2023-02-28 00:32:57 +0100

    audo config data improved

--- UdrProject/Assets/Scripts/Services/AudioService/AudioConfig/AudioConfigData.cs
+++ UdrProject/Assets/Scripts/Services/AudioService/AudioConfig/AudioConfigData.cs
@@ -7,8 +7,21 @@ namespace Urd.Audio
     {
         [field: SerializeField]
         public AudioTypes AudioType { get; private set; }
-        
         [field: SerializeField]
         public AudioClip Clip { get; private set; }
+        [field: SerializeField, Range(0f,1f)]
+        public float Volume { get; private set; }
+        [field: SerializeField]
+        public bool IsLoopable { get; private set; }
+        [field: SerializeField]
+        public string AudioMixerName { get; private set; }
+        [field: SerializeField, Header("Fades")]
+        public float FadeIn { get; private set; }
+        [field: SerializeField]
+        public float PauseFadeIn { get; private set; }
+        [field: SerializeField]
+        public float PauseFadeOut { get; private set; }
+        [field: SerializeField]
+        public float FadeOut { get; private set; }
     }
 }
