716a7e454e4e5ef5bb3ce2edc61bacd414b03626
AntonioZu <antoniojesusnc@gmail.com>
2023-05-25 01:40:31 +0200

    collision detection

--- UdrProject/Assets/UrdPackage/Runtime/Scripts/Services/PhysicsService/Managers/PhysicsAreaBoxManager.cs
+++ UdrProject/Assets/UrdPackage/Runtime/Scripts/Services/PhysicsService/Managers/PhysicsAreaBoxManager.cs
@@ -10,6 +10,13 @@ namespace Urd.Services.Physics
         {
             PrintDebugObject(originPoint, direction, areaShapeModel);
             
+            var areaShapeBoxModel = areaShapeModel as AreaShapeBoxModel;
+            
+            var targets = Physics2D.OverlapBoxAll(originPoint, areaShapeBoxModel.Area, Vector3.Angle(Vector2.up, direction));
+            for (int i = 0; i < targets.Length; i++)
+            {
+                Debug.Log(targets[i]);
+            }
             return false;
         }
     }
--- UdrProject/Assets/UrdPackage/Runtime/Scripts/Services/PhysicsService/Managers/PhysicsAreaConeManager.cs
+++ UdrProject/Assets/UrdPackage/Runtime/Scripts/Services/PhysicsService/Managers/PhysicsAreaConeManager.cs
@@ -8,13 +8,27 @@ namespace Urd.Services.Physics
     public class PhysicsAreaConeManager : PhysicsAreaShapeManager
     {
         public override AreaShapeType AreaShape => AreaShapeType.Cone;
-
-
-
         public override bool TryHit(Vector2 originPoint, Vector2 direction, IAreaShapeModel areaShapeModel)
         {
             PrintDebugObject(originPoint, direction, areaShapeModel);
+            
+            var areaConeShapeModel = areaShapeModel as AreaShapeConeModel; 
 
+            var targets = Physics2D.OverlapCircleAll(originPoint, areaConeShapeModel.Distance);
+            
+            for (int i = 0; i < targets.Length; i++)
+            {
+                Transform target = targets[i].transform;
+                Vector3 dirToTarget = (target.position - (Vector3)originPoint).normalized;
+                if (Vector3.Angle(direction, dirToTarget) < areaConeShapeModel.AngleDegreesClockWise * 0.5f)
+                {
+                    if (Physics2D.Raycast(originPoint, dirToTarget))
+                    {
+                        Debug.Log(target.name);
+                    }
+                }
+            }
+            
             return false;
         }
     }
